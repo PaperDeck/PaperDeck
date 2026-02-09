4b3acc49d97c3f6d304a50f94b534f8254bce11a
prontLin <pront@prontlin.com>
2026-02-09 20:45:09 +0800

    feat(feed): handle new feed with react-hook-form

--- src/renderer/pages/NewFeed.tsx
+++ src/renderer/pages/NewFeed.tsx
@@ -10,28 +10,33 @@ import { Button } from "@/renderer/components/ui/button"
 import { useState } from "react"
 import { useFeedParser } from "../hooks/useApi"
 import { toast } from "react-hot-toast"
+import { useForm, Controller } from "react-hook-form"
+
+interface FormData {
+  url: string
+}
 
 export default function NewFeed() {
   const { t } = useTranslation()
-  const [feedUrl, setFeedUrl] = useState("")
   const [isButtonDisabled, setIsButtonDisabled] = useState(true)
   const [isLoading, setIsLoading] = useState(false)
   const feedParser = useFeedParser()
-  const handleAddFeed = async () => {
-    console.log("Add feed:", feedUrl)
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      url: "",
+    },
+  })
+  const handleAddFeed = async (data: FormData) => {
+    console.log("Add feed:", data.url)
     let result = null
     setIsLoading(true)
     try {
-      result = await feedParser(feedUrl)
+      result = await feedParser(data.url)
     } catch (_) {
       toast.error(t("feedParsingFailed"))
     }
     setIsLoading(false)
     console.log("Parsed feed:", result)
-  }
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFeedUrl(e.target.value.trim())
-    setIsButtonDisabled(e.target.value.trim() === "")
   }
   return (
     <div className="flex items-center justify-center h-full">
@@ -41,20 +46,33 @@ export default function NewFeed() {
           <CardDescription>{t("newFeedDescription")}</CardDescription>
         </div>
         <CardContent>
-          <Input
-            value={feedUrl}
-            onChange={handleInputChange}
-            placeholder="https://example.com/rss"
-            className="w-full"
-            readOnly={isLoading}
-          />
-          <Button
-            className="w-full mt-4"
-            onClick={handleAddFeed}
-            disabled={isButtonDisabled || isLoading}
-          >
-            {isLoading ? t("loading") : t("next")}
-          </Button>
+          <form onSubmit={handleSubmit(handleAddFeed)}>
+            <Controller
+              name="url"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  placeholder="https://example.com/rss"
+                  className="w-full"
+                  readOnly={isLoading}
+                  {...field}
+                  onChange={(e) => {
+                    e.target.value = e.target.value.trim()
+                    field.onChange(e)
+                    setIsButtonDisabled(e.target.value === "")
+                  }}
+                />
+              )}
+            />
+
+            <Button
+              className="w-full mt-4"
+              disabled={isLoading || isButtonDisabled}
+              type="submit"
+            >
+              {isLoading ? t("loading") : t("next")}
+            </Button>
+          </form>
         </CardContent>
       </Card>
     </div>
