1555a6d5b1f6ff29c89592291e916fbf0be17ec8
prontLin <pront@prontlin.com>
2026-02-09 20:22:45 +0800

    feat(feed): attempt to fetch when adding feed and show toast notification on failure

--- src/renderer/pages/NewFeed.tsx
+++ src/renderer/pages/NewFeed.tsx
@@ -6,10 +6,33 @@ import {
   CardTitle,
 } from "@/renderer/components/ui/card"
 import { Input } from "@/renderer/components/ui/input"
-import { Button } from "../components/ui/button"
+import { Button } from "@/renderer/components/ui/button"
+import { useState } from "react"
+import { useFeedParser } from "../hooks/useApi"
+import { toast } from "react-hot-toast"
 
 export default function NewFeed() {
   const { t } = useTranslation()
+  const [feedUrl, setFeedUrl] = useState("")
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+  const feedParser = useFeedParser()
+  const handleAddFeed = async () => {
+    console.log("Add feed:", feedUrl)
+    let result = null
+    setIsLoading(true)
+    try {
+      result = await feedParser(feedUrl)
+    } catch (_) {
+      toast.error(t("feedParsingFailed"))
+    }
+    setIsLoading(false)
+    console.log("Parsed feed:", result)
+  }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFeedUrl(e.target.value.trim())
+    setIsButtonDisabled(e.target.value.trim() === "")
+  }
   return (
     <div className="flex items-center justify-center h-full">
       <Card className="flex flex-col px-5 py-10 w-full max-w-md">
@@ -18,8 +41,20 @@ export default function NewFeed() {
           <CardDescription>{t("newFeedDescription")}</CardDescription>
         </div>
         <CardContent>
-          <Input placeholder="https://example.com/rss" className="w-full" />
-          <Button className="w-full mt-4">{t("next")}</Button>
+          <Input
+            value={feedUrl}
+            onChange={handleInputChange}
+            placeholder="https://example.com/rss"
+            className="w-full"
+            readOnly={isLoading}
+          />
+          <Button
+            className="w-full mt-4"
+            onClick={handleAddFeed}
+            disabled={isButtonDisabled || isLoading}
+          >
+            {isLoading ? t("loading") : t("next")}
+          </Button>
         </CardContent>
       </Card>
     </div>
