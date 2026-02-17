7eacbf26d15eda0f63d4366e9c2a49e00e36f7d5
prontLin <pront@prontlin.com>
2026-02-17 18:23:01 +0800

    feat(ui): add ScrollArea component and use it in NewFeed and Articles components

--- src/renderer/layouts/Main.tsx
+++ src/renderer/layouts/Main.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router"
+import { ScrollArea } from "@/renderer/components/ui/scroll-area"
 
 export default function MainLayout() {
   return (
-    <div className="bg-zinc-50 dark:bg-zinc-900 h-screen overflow-auto">
+    <ScrollArea className="bg-zinc-50 dark:bg-zinc-900 h-screen overflow-auto">
       <Outlet></Outlet>
-    </div>
+    </ScrollArea>
   )
 }
