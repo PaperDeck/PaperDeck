59d096a4526d222912df2050c2e0849e48da0caa
prontLin <pront@prontlin.com>
2026-02-21 19:46:01 +0800

    feat(articles-list): add filter dropdown for "All" and "Unread" articles

--- src/renderer/pages/ArticlesList.tsx
+++ src/renderer/pages/ArticlesList.tsx
@@ -3,7 +3,7 @@ import { useArticleService, useFeedSyncService } from "@/renderer/hooks/useApi"
 import useRelativeTime from "@/renderer/hooks/useRelativeTime"
 import truncateText from "@/renderer/utils/truncateText"
 import extractText from "@/renderer/utils/extractText"
-import { RefreshCcw } from "lucide-react"
+import { RefreshCcw, ListFilter, Check } from "lucide-react"
 import IconButton from "@/renderer/components/IconButton"
 import {
   Tooltip,
@@ -17,26 +17,26 @@ import { useNavigate } from "react-router"
 import useScrollRestoration from "@/renderer/hooks/useScrollRestoration"
 import { cn } from "@/renderer/lib/utils"
 import type { ArticleWithFeed } from "@/shared/types/article"
+import {
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuItem,
+} from "@/renderer/components/ui/dropdown-menu"
 export default function ArticlesList() {
   const articleService = useArticleService()
   const feedSyncService = useFeedSyncService()
   const [isLoading, setIsLoading] = useState(false)
+  const [filter, setFilter] = useState<"all" | "unread">("unread")
   const { t } = useTranslation()
   const { articles, setArticles } = useArticles()
   const navigate = useNavigate()
   const fromNow = useRelativeTime()
   useScrollRestoration("articles-list")
-  const handleRefresh = async () => {
-    setIsLoading(true)
-    const result = await feedSyncService.syncFeeds()
-
-    //TODO: Show sync result in UI instead of console
-    console.log(
-      `Sync result: ${result.data.successCount} feeds synced successfully, ${result.data.errorCount} feeds failed to sync.`,
-    )
+  const getArticles = async (ignoreRead: boolean) => {
     const articles = await articleService.getAll({
       includeFeeds: true,
-      ignoreRead: true,
+      ignoreRead: ignoreRead,
     })
     if (articles.success) {
       setArticles(articles.data)
@@ -46,6 +46,15 @@ export default function ArticlesList() {
         articles.error,
       )
     }
+  }
+  const handleRefresh = async () => {
+    setIsLoading(true)
+    const result = await feedSyncService.syncFeeds()
+    await getArticles(filter === "unread")
+    //TODO: Show sync result in UI instead of console
+    console.log(
+      `Sync result: ${result.data.successCount} feeds synced successfully, ${result.data.errorCount} feeds failed to sync.`,
+    )
     setIsLoading(false)
   }
   const handleArticleClick = (article: ArticleWithFeed) => {
@@ -55,10 +64,15 @@ export default function ArticlesList() {
       article.isRead = true
     }, 100)
   }
+  const handleFilterChange = async (newFilter: "all" | "unread") => {
+    if (newFilter === filter) return
+    setFilter(newFilter)
+    await getArticles(newFilter === "unread")
+  }
   return (
     <div>
       <div className="flex flex-col items-center pt-10">
-        <div className="flex mb-10">
+        <div className="flex mb-5">
           <Tooltip>
             <TooltipTrigger asChild>
               <IconButton onClick={handleRefresh} disabled={isLoading}>
@@ -70,6 +84,45 @@ export default function ArticlesList() {
             </TooltipTrigger>
             <TooltipContent>{t("refreshFeeds")}</TooltipContent>
           </Tooltip>
+        </div>
+        <div className="w-md mb-1 ml-5">
+          <DropdownMenu>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <DropdownMenuTrigger asChild>
+                  <IconButton>
+                    <ListFilter size={24} />
+                  </IconButton>
+                </DropdownMenuTrigger>
+              </TooltipTrigger>
+              <TooltipContent>{t("filterArticles")}</TooltipContent>
+            </Tooltip>
+            <DropdownMenuContent
+              className="flex flex-col whitespace-nowrap w-40"
+              onCloseAutoFocus={(e) => e.preventDefault()}
+            >
+              <DropdownMenuItem
+                className={cn(filter === "all" && "font-bold")}
+                onSelect={() => handleFilterChange("all")}
+              >
+                <Check
+                  size={16}
+                  className={filter === "all" ? "" : "opacity-0"}
+                />
+                {t("allArticles")}
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className={cn(filter === "unread" && "font-bold")}
+                onSelect={() => handleFilterChange("unread")}
+              >
+                <Check
+                  size={16}
+                  className={filter === "unread" ? "" : "opacity-0"}
+                />
+                {t("unreadArticles")}
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
         <div>
           {!articles &&
@@ -94,7 +147,7 @@ export default function ArticlesList() {
               key={article.id}
               className={cn(
                 "flex flex-col items-start p-5 mb-4 w-full min-w-sm max-w-md rounded-lg hover:shadow-lg hover:bg-zinc-100 dark:hover:bg-zinc-800 transition-all duration-250 cursor-pointer text-start",
-                article.isRead && "opacity-60",
+                article.isRead && filter === "unread" && "opacity-60",
               )}
               onClick={() => handleArticleClick(article)}
             >
