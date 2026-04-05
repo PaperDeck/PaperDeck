dbc2697ec006612083075dbf904de7b149d6d85a
prontLin <pront@prontlin.com>
2026-04-05 21:17:29 +0800

    feat(useRelativeTime): refactor fromNow function to use useCallback

--- src/renderer/hooks/useRelativeTime.ts
+++ src/renderer/hooks/useRelativeTime.ts
@@ -1,16 +1,20 @@
 import { useTranslation } from "react-i18next"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
+import { useCallback } from "react"
 
 dayjs.extend(relativeTime)
 
 const useRelativeTime = () => {
   const { i18n } = useTranslation()
 
-  const fromNow = (date: string | number | Date) => {
-    const currentLang = i18n.language.toLowerCase()
-    return dayjs(date).locale(currentLang).fromNow()
-  }
+  const fromNow = useCallback(
+    (date: string | number | Date) => {
+      const currentLang = i18n.language.toLowerCase()
+      return dayjs(date).locale(currentLang).fromNow()
+    },
+    [i18n.language],
+  )
 
   return fromNow
 }
